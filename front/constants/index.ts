a60e41e461401931d8f6edd4614c2394453bbe95
gaetan cabaret <gaetan@e36knots.com>
2024-07-03 12:46:25 +0200

    fix(contract): deploy on sepolia;add env archive

--- front/constants/index.ts
+++ front/constants/index.ts
@@ -1,3 +1,4 @@
-export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const contractAddress = "0xE8C5f757a75AaBeeF6B4450cf5a56B584d5BF524";
+export const publicContractAddress = "0xE8C5f757a75AaBeeF6B4450cf5a56B584d5BF524";
 import compiledJson from "../hardhat/artifacts/contracts/Emmental.sol/EmmentalCollection.json";
 export const contractAbi = compiledJson.abi;
--- front/hardhat/hardhat.config.ts
+++ front/hardhat/hardhat.config.ts
@@ -1,13 +1,22 @@
 import type { HardhatUserConfig } from "hardhat/config";
+
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomiclabs/hardhat-ethers";
+
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-  networks: {
-    localhost:{
-      url: "http://127.0.0.1:8545"
-    }
-  }
+    solidity: "0.8.24",
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545",
+        },
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_TOKEN}`,
+            accounts: [`${process.env.SEPOLIA_PRIVATE_KEY}`],
+        },
+    },
 };
 
 export default config;
